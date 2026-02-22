7098aed56c29c2e32e1e2d4f1322c94eb34798c8
desenvolvedorwebV <50739488+desenvolvedorwebV@users.noreply.github.com>
2026-02-22 03:58:02 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME="tetris-vilma-v1";
+
+const FILES=[
+"./",
+"./index.html",
+"./manifest.json",
+"./icon-192.png",
+"./icon-512.png"
+];
+
+self.addEventListener("install",(e)=>{
+
+e.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache=>cache.addAll(FILES))
+);
+
+});
+
+self.addEventListener("fetch",(e)=>{
+
+e.respondWith(
+
+caches.match(e.request)
+.then(resp=> resp || fetch(e.request))
+
+);
+
+});
